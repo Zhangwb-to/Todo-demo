7c64e00a9440a9237cf0d2d4e7d1281dfe87c376
Zhanwb-to <1107207364@qq.com>
2020-11-16 20:11:17 +0800

    1

--- README.md
+++ README.md
@@ -1,24 +1,4 @@
-# todo-app
+#### 基于vue 3 创建的demo
 
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+#### Todo_app
+简单的使用示例，基于vue3的composition API 编写
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <div class="container">
+      <h1>欢迎使用Todo!</h1>
+      <todo-add :tid="todos.length" @add-todo="addTodo"/>
+      <todo-filter :selected="filter" @change-filter="filter = $event" />
+      <todo-list :todos="filteredTodos"/>
+    </div>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoAdd from '@/components/TodoAdd'
+import TodoFilter from '@/components/TodoFilter'
+import TodoList from '@/components/TodoList'
+import useTodos from "@/composables/useTodos"
+import useFiltersTodo from '@/composables/useFiltersTodo'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodoAdd,
+    TodoFilter,
+    TodoList
+  },
+  setup() {
+    const { todos, addTodo } = useTodos();
+    const { filter, filteredTodos } = useFiltersTodo(todos);
+    return {
+      todos,
+      addTodo,
+      filter,
+      filteredTodos
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: Helvetica, "PingFang SC", "Microsoft Yahei", sans-serif;
 }
+main {
+  width: 100vw;
+  min-height: 100vh;
+  display: grid;
+  align-items: center;
+  justify-items: center;
+  background-color: rgb(203,210,240);
+}
+.container {
+  width: 94%;
+  max-width: 400px;
+  box-shadow: 0 0 12px rgba(0, 0, 0, 0.15);
+  padding: 48px 24px;
+  border-radius: 12px;
+  background-color: rgb(245, 246, 252);
+}
+
+/* 标题 */
+h1 {
+  margin: 26px 0;
+  font-size: 28px;
+  color: #414873;
+}
+
 </style>
